33a47f7a89c78846b9fdff18ebc0ecd440877dc6
Andrei <ashipulev1977@gmail.com>
2020-01-20 22:36:49 +0600

    Versia 6 1. Убрали расчет токи и мощность и вместо поставили таблицу расчета сечения кабеля; 2. На экране menu_vibor_screen все повторяющиеся контейнера объединил в один виджет 3. В Калькулятор различных величин добавил что если nameValue==null то присвоить Выберите энергоноситель 4. Добавили страничку Техника безопасности с выбором по наименованию

--- lib/spravochniki/main_drawer/texnica_bezopasnosti.dart
+++ lib/spravochniki/main_drawer/texnica_bezopasnosti.dart
@@ -1,10 +1,13 @@
 import 'dart:ui' as prefix0;
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gradient_app_bar/gradient_app_bar.dart';
 import 'package:spravochnik_energi/menu_vibor_screei.dart';
 import 'dart:math';
 import 'dart:core';
+
+import 'package:spravochnik_energi/spravochniki/sprav_energonosit.dart';
 
 class TexnikaBezopasnosti extends StatefulWidget {
   @override
@@ -12,7 +15,6 @@ class TexnikaBezopasnosti extends StatefulWidget {
 }
 
 class _TexnikaBezopasnostiState extends State<TexnikaBezopasnosti> {
-
   //задаем ключ
   final _scaffoldKey = GlobalKey<ScaffoldState>();
 
@@ -21,32 +23,86 @@ class _TexnikaBezopasnostiState extends State<TexnikaBezopasnosti> {
   double height_tabl = 70; //высота ячейки таблицы
 
   String
-  nString; // текстовая переменная для перевода введенных значений в число
+      nString; // текстовая переменная для перевода введенных значений в число
 
   // создаем контроллеры
   TextEditingController valueFirst =
-  TextEditingController(); // контроллер для сохранения величины вводимого значения
+      TextEditingController(); // контроллер для сохранения величины вводимого значения
 
-
-  //повторяющийся виджет столбцов с лева в таблице
-  Widget tablLeftComponent(
-      String nameTablLeftComp, double h_shript, double h_megduStrok) {
+  //повторяющийся виджет основных столбцов  в таблице
+  Widget tablRowComponent(String nomerPoz, String nameTablComp, nameScreen,
+      {double h_shript = 1.2, double h_megduStrok = 1.0}) {
+//    return TableCell(
+//        children: <Widget>[
+//          Stack(children: <Widget>[
+//            Positioned(
+//              child: Container(
+////        height: height_tabl,
+//                padding: EdgeInsets.all(8),
+//                child: Text(
+//                  nameTablLeftComp,
+//                  textScaleFactor: h_shript,
+//                  textAlign: TextAlign.left,
+//
+//                  style: TextStyle(
+//                    fontWeight: FontWeight.bold,
+//                    fontStyle: FontStyle.italic,
+//                    color: Colors.black87,
+//                    height: h_megduStrok,
+//                  ),
+//                ),
+//              ),
+//            ),
+//            Positioned(
+//
+//            )
+//
+//          ],)
+//
+//  ]
+//    );
     return TableCell(
-      child: Container(
-        height: height_tabl,
-        padding: EdgeInsets.all(8),
-        child: Text(
-          nameTablLeftComp,
-          textScaleFactor: h_shript,
-          textAlign: TextAlign.left,
-          style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontStyle: FontStyle.italic,
-              color: Colors.black87,
-              height: h_megduStrok),
+        child: Container(
+      padding: EdgeInsets.symmetric(vertical: 2, horizontal: 10),
+      child: InkWell(
+        child: Row(
+          children: <Widget>[
+
+            RichText(
+              //Данный виджет используется, если необходимо отобразить в тексте несколько стилей. Каждый отдельный текст описывается с помощью TextSpan, в котором мы указываем необходимые нам стили. Для того чтобы не усложнять визуально код, старайтесь использовать для основного текста — стиль принятый по умолчанию, а для отличия — TextSpan:
+              text: TextSpan(
+                text: nomerPoz,
+                style: TextStyle(
+                  inherit: true,
+                  color: Colors.black.withOpacity(1.0),
+                  /* при использовании Paint color не использовать*/
+                  fontSize: 20,
+//                foreground: Paint()..style=PaintingStyle.stroke..strokeWidth=2..color=Colors.purple[700],// Делает обводку и границы буквы
+                ),
+              ),
+            ),
+            Flexible(//если вам придется работать с текстом, который выходит за рамки контейнера, вы можете обернуть виджет RichText или Text в виджет Flexible.
+              child:  RichText(
+              text: TextSpan(
+                text: nameTablComp,
+                style: TextStyle(
+                  inherit: true,
+                  color: Colors.black.withOpacity(1.0),
+                  /* при использовании Paint color не использовать*/
+                  fontSize: 20,
+//                foreground: Paint()..style=PaintingStyle.stroke..strokeWidth=2..color=Colors.purple[700],// Делает обводку и границы буквы
+                ),
+              ),
+            ),)
+
+          ],
         ),
+        onTap: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => nameScreen));
+        },
       ),
-    );
+    ));
   }
 
   //повторяющийся виджет столбцов по середине и справа в таблице
@@ -70,20 +126,17 @@ class _TexnikaBezopasnostiState extends State<TexnikaBezopasnosti> {
     );
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldKey,
       appBar: GradientAppBar(
         title: Text(
-              "Техника безопасности в электроустановках",
+          "Техника безопасности в электроустановках",
           textScaleFactor: 0.8,
           textAlign: TextAlign.center,
           maxLines: 10,
           style: TextStyle(color: Colors.black),
-
         ),
         backgroundColorStart: Colors.cyanAccent,
         backgroundColorEnd: Colors.tealAccent,
@@ -99,9 +152,7 @@ class _TexnikaBezopasnostiState extends State<TexnikaBezopasnosti> {
           IconButton(
             icon: Icon(Icons.block),
             onPressed: () {
-              setState(() {
-
-              });
+              setState(() {});
             },
           ),
         ],
@@ -122,145 +173,139 @@ class _TexnikaBezopasnostiState extends State<TexnikaBezopasnosti> {
           children: <Widget>[
             //-----------Таблица вводных данных---------------
             Table(
-              border: TableBorder.all(),
+              border: TableBorder.all(width: 3, color: Colors.deepPurple),
               defaultColumnWidth: FixedColumnWidth(50.0),
               children: [
                 TableRow(//первая строка таблицы
                     children: <TableCell>[
-                      tablLeftComponent("Наименование величины", 1.3, 1.5),
-                      tablCentrRightComponent(
-                          "Значение в мм2", 1.3, 2.3)
-                    ]),
-                TableRow(//вторая строка таблицы
-                    children: <TableCell>[
-                      tablLeftComponent(
-                          "Сечение проводника",
-                          1.3,
-                          2.3),
-                      TableCell(
-                        child: TextFormField(
-                          controller: valueFirst,
-                          keyboardType: TextInputType.number,
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontStyle: FontStyle.italic,
-                              color: Colors.black87,
-                              height: 2.3),
-                          decoration: InputDecoration(
-                            contentPadding: EdgeInsets.all(15.0),
-                            hintText: "Введите значение",
-                          ),
-                        ),
-                      ),
-                    ]),
-              ],
-            ),
-            //------- кнопка расчета ------------
-            Container(
-              width: 250,
-              height: 70,
-              margin: EdgeInsets.all(2),
-              decoration: BoxDecoration(
-                  color: Colors.blueGrey,
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  border: Border.all(width: 2),
-                  gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      stops: [0.2, 0.9],
-                      colors: [Colors.indigo, Colors.blueAccent])),
-              child: RaisedButton(
-                elevation:
-                3.0, //убераем у нее тень (или цифрами указывается сколько пикселей с верху вниз убрать тень)
-                color: Colors.transparent,
-                child: Text(
-                  "Расчитать",
-                  textScaleFactor: 1.5,
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.italic,
-                      color: Colors.black87),
-                ),
-                onPressed: () {
-                  if (valueFirst.text == "") {
-                    _scaffoldKey.currentState.showSnackBar(SnackBar(
-                      content:
-                      Text("Вы не ввели кол-во, Пожалуйста введите кол-во"),
-                    ));
-                  } else {
-                    setState(() {
-                      nString = valueFirst.text;
-
-                    });
-                  }
-                },
-              ),
-//              ),
-            ),
-            //---------- таблица выводных данных -------------
-            Table(
-              //вторая таблица с результатами
-              border: TableBorder.all(),
-              defaultColumnWidth: FixedColumnWidth(50.0),
-              children: [
+                  tablRowComponent(
+                      "1. ",
+                      "Назначение и область применения правил.",
+                      TexnikaBezopasnosti()),
+                ]),
                 TableRow(//первая строка таблицы
-
                     children: <TableCell>[
-                      tablLeftComponent(
-                          "При сечении, мм2",
-                          1.3,
-                          2.3),
-                      tablCentrRightComponent(
-                          "При гпгвггот",
-                          1.3,
-                          2.3)
-                    ]),
-                TableRow(//
-
+                  tablRowComponent(
+                      "2. ",
+                      "Требования к техническому персоналу.",
+                      TexnikaBezopasnosti()),
+                ]),
+                TableRow(//первая строка таблицы
                     children: <TableCell>[
-                      tablLeftComponent(
-                          "Диаметр =  , мм",
-                          1.3,
-                          2.3),
-                      tablCentrRightComponent(
-                          "сууаавйаПв  , мм",
-                          1.3,
-                          2.3) //выводим значения и ограничиваем кол-во знаков после запятой =2
-                    ]),
+                  tablRowComponent(
+                      "3. ",
+                      "Классификация электроустановок компании.",
+                      TexnikaBezopasnosti()),
+                ]),
+                TableRow(//первая строка таблицы
+                    children: <TableCell>[
+                  tablRowComponent(
+                      "4. ",
+                      "Требования к помещениям. \n Размещение оборудования.",
+                      TexnikaBezopasnosti()),
+                ]),
+                TableRow(//первая строка таблицы
+                    children: <TableCell>[
+                  tablRowComponent(
+                      "5. ",
+                      "Требования безопасности к Радиоустановкам.",
+                      TexnikaBezopasnosti()),
+                ]),
+                TableRow(//первая строка таблицы
+                    children: <TableCell>[
+                  tablRowComponent(
+                      "6. ",
+                      "Требования к применению Средств защиты.",
+                      TexnikaBezopasnosti()),
+                ]),
+                TableRow(//первая строка таблицы
+                    children: <TableCell>[
+                  tablRowComponent("7. ", "ЗУ - заземление электроустановок.",
+                      TexnikaBezopasnosti()),
+                ]),
+                TableRow(//первая строка таблицы
+                    children: <TableCell>[
+                  tablRowComponent(
+                      "8. ",
+                      "Оперативное обслуживание электроустановок.",
+                      TexnikaBezopasnosti()),
+                ]),
+                TableRow(//первая строка таблицы
+                    children: <TableCell>[
+                  tablRowComponent(
+                      "9. ",
+                      "Производство работ в электроустановках.",
+                      TexnikaBezopasnosti()),
+                ]),
+                TableRow(//первая строка таблицы
+                    children: <TableCell>[
+                  tablRowComponent(
+                      "10. ",
+                      "ОРГАНИЗАЦИОННЫЕ МЕРОПРИЯТИЯ, обеспечивающие безопасность работ.",
+                      TexnikaBezopasnosti()),
+                ]),
+                TableRow(//первая строка таблицы
+                    children: <TableCell>[
+                  tablRowComponent(
+                      "11. ",
+                      "ТЕХНИЧЕСКИЕ МЕРОПРИЯТИЯ, "
+                          "обеспечивающие безопасность работ, выполняемых со снятием   без снятия "
+                          "напряжения на токоведущих частях и вблези них.",
+                      TexnikaBezopasnosti()),
+                ]),
+                TableRow(//первая строка таблицы
+                    children: <TableCell>[
+                  tablRowComponent(
+                      "12. ",
+                      "АВАРИЙНО - ВОССТАНОВИТЕЛЬНЫЕ РАБОТЫ.",
+                      TexnikaBezopasnosti()),
+                ]),
+                TableRow(//первая строка таблицы
+                    children: <TableCell>[
+                  tablRowComponent("", " ", TexnikaBezopasnosti()),
+                ]),
+                TableRow(//первая строка таблицы
+                    children: <TableCell>[
+                  tablRowComponent(
+                      "16. ", "АККУМУЛЯТОРНЫЕ БАТАРЕИ.", TexnikaBezopasnosti()),
+                ]),
+                TableRow(//первая строка таблицы
+                    children: <TableCell>[
+                  tablRowComponent(
+                      "17. ",
+                      "17. ПОГРУЗОЧНО - РАЗГРУЗОЧНЫЕ РАБОТЫ.",
+                      TexnikaBezopasnosti()),
+                ]),
+                TableRow(//первая строка таблицы
+                    children: <TableCell>[
+                  tablRowComponent(
+                      "18. ", "РАБОТЫ НА ВЫСОТЕ.", TexnikaBezopasnosti()),
+                ]),
+                TableRow(//первая строка таблицы
+                    children: <TableCell>[
+                  tablRowComponent(
+                      "19. ",
+                      "Требования безопасности при работе С ЭЛЕКТРОИНСТРУМЕНТОМ, "
+                          "ручными электрическими машинами и ручными электрическими светильниками.",
+                      TexnikaBezopasnosti()),
+                ]),
+                TableRow(//первая строка таблицы
+                    children: <TableCell>[
+                  tablRowComponent(
+                      "20. ",
+                      "ПРОВЕДЕНИЕ ИСПЫТАНИЙ ОБОРУДОВАНИЯ И ИЗМЕРЕНИЙ.",
+                      TexnikaBezopasnosti()),
+                ]),
+                TableRow(//первая строка таблицы
+                    children: <TableCell>[
+                  tablRowComponent("21. ", "ПЛАКАТЫ И ЗНАКИ БЕЗОПАСНОСТИ.",
+                      TexnikaBezopasnosti()),
+                ]),
               ],
             ),
-            //-------- контейнер с названием таблицы пропускной способности ----------
-            Container(
-              width: 250,
-              height: 500,
-              margin: EdgeInsets.all(0),
-              decoration: BoxDecoration(
-                color: Colors.transparent,
-                border: Border.all(width: 1),
-              ),
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: horisont_padd, vertical: vertical_padd),
-                decoration: BoxDecoration(
-                  border: Border.all(color: color_border,width: width_border),
-                  borderRadius: BorderRadius.all(Radius.circular(radius_border)),
-                  //color: color_borderRadius,
-                ),
-                width: width_contaner,
-                height: height_contaner,
-                margin: EdgeInsets.all(1), //отступы со всех сторон
-                child: FittedBox(
-                  child: Image.asset("assets/propusknaiya_sposobnost.jpg", alignment: Alignment.bottomCenter,),
-                  fit: BoxFit.fill,
-                ),
-              ),
-//              ),
-            ),
-            //-------- таблица пропускной способности проводников -------
           ],
         ),
       ),
     );
   }
 }
-
